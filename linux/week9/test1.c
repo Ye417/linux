5f95d829d757378d1487c371dfec065538fe7674
Ye417 <1183356177@qq.com>
2019-11-11 03:11:51 -0800

    week9

---
+++ linux/week9/test1.c
@@ -0,0 +1,26 @@
+#include "my.h"
+static void __attribute__ ((constructor)) before_main()
+{
+  printf("Running before mian!\n");
+}
+static void callback1()
+{
+  printf("1:Running after mian!\n");
+}
+static void callback2()
+{
+  printf("2:Running after mian!\n");
+}
+static void callback3()
+{
+  printf("3:Running after mian!\n");
+}
+int main()
+{
+  atexit(callback1);
+  atexit(callback2);
+  atexit(callback3);
+  printf("test1:pid=%d,ppid= %d\n",getpid(),getppid());
+  sleep(2);
+}
+
